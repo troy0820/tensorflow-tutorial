3f79f5d4e19ca075003c7cad044b5e72962a016c
Troy Connor <troy0820@users.noreply.github.com>
2016-06-29 13:36:17 -0400

    [factorial]Created factorial function in python :boom:

---
+++ factorial.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+import sys
+
+def factorial(number):
+	if(number == 1):
+		return number
+	return number * factorial(number - 1)
+
+if (len(sys.argv) > 1):
+    x = int(sys.argv[1])
+else:
+    x = input('Enter a number for the factorial ')
+
+y = factorial(x)
+
+print('Factorial of 5 is {0}'.format(y))
